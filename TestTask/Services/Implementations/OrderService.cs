3bea31c456857e9fb72d2ba7d9805e5ed3333005
opvlinkup <146995351+opvlinkup@users.noreply.github.com>
2024-04-16 04:43:59 +0300

    add solution

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,73 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+    
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        } 
+        private async Task<Order> GetMaxSumOrder()
+        {
+            var query = _context.Orders
+                .OrderByDescending(o => o.Price * o.Quantity)
+                .Select(o => new Order
+                {
+                    Id = o.Id,
+                    ProductName = o.ProductName,
+                    Price = o.Price,
+                    Quantity = o.Quantity,
+                    UserId = o.UserId,
+                    User = new User
+                    {
+                        Id = o.User.Id,
+                        Email = o.User.Email,
+                        Status = o.User.Status
+                    }
+                });
+
+            var maxSumOrder = await query.FirstOrDefaultAsync();
+            return maxSumOrder;
+
+        }
+
+        private async Task<List<Order>> GetOrdersQuantityGreaterThanTen()
+        {
+            var query = _context.Orders
+                .Where(o => o.Quantity > 10)
+                .Select(o => new Order
+                {
+                    Id = o.Id,
+                    ProductName = o.ProductName,
+                    Price = o.Price,
+                    Quantity = o.Quantity,
+                    UserId = o.UserId,
+                    User = new User
+                    {
+                        Id = o.User.Id,
+                        Email = o.User.Email,
+                        Status = o.User.Status
+                    }
+                });
+
+            var orders = await query.ToListAsync();
+            return orders;
+        }
+        public async Task<Order> GetOrder()
+        {
+            return await GetMaxSumOrder();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await GetOrdersQuantityGreaterThanTen();
+        }
+    }
+    
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<User> GetUser()
+        {
+            return await _context.Users
+                .OrderByDescending(u => u.Orders.Sum(o => o.Quantity)).Select(u => new
+                User
+                {
+                    Id = u.Id,
+                    Email = u.Email,
+                    Status = u.Status,
+                    Orders = _context.Orders.Where(o => o.UserId == u.Id).ToList()
+                })
+                .FirstOrDefaultAsync();
+        }
+
+         private async Task<List<User>> GetInactiveUsers()
+          {
+              return await _context.Users.Where(u => u.Status == UserStatus.Inactive).Select(u => new
+                  User
+                  {
+                      Id = u.Id,
+                      Email = u.Email,
+                      Status = u.Status,
+                      Orders = _context.Orders.Where(o => o.UserId == u.Id).ToList()
+                  }).ToListAsync();
+          } 
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await GetInactiveUsers();
+        }
+    }
+
+}
