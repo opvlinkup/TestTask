6b2d6be1ad40831c3cab981bf47ac0456709253d
opvlinkup <146995351+opvlinkup@users.noreply.github.com>
2024-11-14 05:30:11 +0300

    first commit

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public async Task<Order> GetOrder()
+        {
+            return await _context.Orders.Where(order => order.Quantity > 1)
+                .OrderByDescending(order => order.CreatedAt).
+                FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _context.Orders.Where(order=>order.User.Status == UserStatus.Active)
+                .OrderBy(order => order.CreatedAt)
+                .ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var userWithMaxTotalGoods2003 = await _context.Users
+                .Include(user => user.Orders.Where(order => order.CreatedAt.Year == 2003)).
+                Select(user =>
+                new
+                {
+                    User = user,
+                    TotalOrders = user.Orders.Where(order => order.CreatedAt.Year == 2003)
+                        .Sum(order => order.Quantity)
+                })
+                .OrderByDescending(seletction => seletction.TotalOrders)
+                .FirstOrDefaultAsync();
+            return userWithMaxTotalGoods2003?.User;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var usersWithPaidOrdersIn2010 = await _context.Users.Where(user =>
+                    user.Orders.Any(order => order.Status == OrderStatus.Paid && order.CreatedAt.Year == 2010))
+                .ToListAsync();
+            return usersWithPaidOrdersIn2010;
+        }
+    }
+}
